ee0421c3854972dd01f9ed610b0d7db088f2d042
Csábrádi, Attila <attila.csabradi.ext@siemens-healthineers.com>
2025-04-09 21:17:42 +0200

    DTOs

---
+++ PlatformService/DTOs/PlatformReadDto.cs
@@ -0,0 +1,12 @@
+namespace PlatformService.DTOs;
+
+public class PlatformReadDto
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Publisher { get; set; }
+
+    public required string Cost { get; set; }
+}
