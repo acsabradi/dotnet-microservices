3f9455b4ea768c525f08cfb85c24c2dcd95479e5
Csábrádi, Attila <attila.csabradi.ext@siemens-healthineers.com>
2025-05-20 23:04:34 +0200

    cluster starter szkript kiegészítve secret létrehozással és DB deployment-el

--- K8S/start-cluster.ps1
+++ K8S/start-cluster.ps1
@@ -1,6 +1,8 @@
 kubectl apply -f .\platforms-depl.yaml
 kubectl apply -f .\commands-depl.yaml
 kubectl apply -f .\platforms-nodeport-srv.yaml
-kubectl apply -f https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.1/deploy/static/provider/cloud/deploy.yaml
+kubectl apply -f "https://raw.githubusercontent.com/kubernetes/ingress-nginx/controller-v1.12.1/deploy/static/provider/cloud/deploy.yaml"
 kubectl apply -f .\ingress-srv.yaml
 kubectl apply -f .\local-pvc.yaml
+kubectl create secret generic mssql --from-literal=SA_PASSWORD="pa55w0rd!"
+kubectl apply -f .\mssql-plat-depl.yaml
